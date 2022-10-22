fe1fc8632eeb321f3484a1008d7fc99a35d429d6
Guilherme Gabriel <guilhermag@gmail.com>
2022-10-22 17:35:51 -0300

    finalizacao do projeto

---
+++ README.md
@@ -0,0 +1,26 @@
+# Abstraindo um Bootcamp Usando Orientação a Objetos em Java
+
+O objetivo desse projeto foi colocar em prática os conhecimentos adquiridos durante o curso da [Digital Innovation One](https://www.dio.me/).
+
+## Tecnologias utilizadas
+
+- Java.
+
+## Como testar o projeto
+
+Com o JDK 11+ instalado, digite no seu terminal:
+
+```bash
+# Clone o repositório do projeto
+git clone https://github.com/guilhermag/dio-bootcamp-poo.git
+
+# Vá para a pasta dos arquivos
+cd dio-bootcamp-poo/src/
+
+# agora é só rodar o arquivo que desejar, por exemplo
+java  Main.java
+```
+
+## Créditos
+
+Desenvolvido por Guilherme de Araujo Gabriel.
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso("como dancar", "curso sobre como dancar", 50);
+        Curso cursoJs = new Curso("Javascript I", "conceitos basicos de javascript", 8);
+        Mentoria mentoriaJava = new Mentoria("Mentoria Java I", "mentoria explicando os conceitos " +
+                "basicos da linguagem");
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Titulo do bootcamp");
+        bootcamp.setDescricao("descricao bootcamp");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(cursoJs);
+        bootcamp.getConteudos().add(mentoriaJava);
+
+        Dev devGuilherme = new Dev("Guilherme");
+        System.out.println(devGuilherme.getConteudosInscritos());
+        devGuilherme.inscreverBootcamp(bootcamp);
+        System.out.println(devGuilherme.getConteudosInscritos());
+
+        Dev devBruna = new Dev("Bruna");
+        System.out.println(devBruna.getConteudosInscritos());
+        devBruna.inscreverBootcamp(bootcamp);
+        System.out.println(devBruna.getConteudosInscritos());
+
+
     }
+
 }
